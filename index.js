091f5a575f946235f031e7aaaa2659451bbbc1ad
Axsiow <Axsiow@gmail.com>
2019-11-26 14:34:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,67 @@
-const Discord = require('discord.js')
-const bot = new Discord.Client()
+				// Include
 
-bot.on('message', function (message) {
-	if (message.content === '!ping') {
-		message.reply('pong')
+const Discord = require("discord.js")
+
+				// Variable
+
+const client = new Discord.Client();
+const PREFIX = "R";
+// const EPSITIER = client.channels("514065264837394442");
+// const condamné = client.channels("648864507342618635");
+
+
+				// Events
+
+				// Ready
+
+client.on("ready",  () => {
+	console.log("Beep boop");
+});
+
+client.on("message", message => {
+	if(message.content[0] === PREFIX) {
+		if(message.content === "R9 TACOS ?"){
+			message.reply("LE TACOS C'EST GRENOBLOIS");
+		}
 	}
-})
+});
 
-bot.login('')
+
+
+				// Ping
+
+client.on("message", message => {
+	if(message.content[0] === PREFIX) {
+		if(message.content === "R9 ping"){
+			// condamné.send("test");
+			message.reply("pong");
+		}
+	}
+});
+
+				// Message perso
+
+client.on("message", message => {
+	if(message.content[0] === PREFIX) {
+		if(message.content === "R9 TEST"){
+			message.channel.send("TEST");
+		}
+	}
+});
+
+				// Message personalisé Théo
+
+// client.on("message", async message => {
+// 	if(message.channel instanceof discord.DMChannel) {
+// 		if (message.author.id == "133294445016645632") {
+// 			console.log("Message envoyé dans le channel !");
+// 			var hep = client.guilds.find(g => g.name === "EPSI TIER").channels.find(c => c.name === "condamné");
+// 				hep.send(`_${message.content}_ `);
+// 		}
+// 	}
+// })
+
+
+				// Connexion
+
+client.login("")
